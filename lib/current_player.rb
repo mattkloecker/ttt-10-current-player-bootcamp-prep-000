6ed23a293df77444f651e4360ca82e6faa07e42d
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-29 18:40:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,10 +3,8 @@
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if space = "X" || "O";
+    if space = X || O;
       counter += 1;
-    else
-      return counter
     end
   end
   return counter
